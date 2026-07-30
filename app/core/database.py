f1a48418adcd160c107cd03f104e56994f866f18
Rija <chaulagain.rija@gmail.com>
2026-07-30 09:05:04 +0545

    basic changes

---
+++ app/core/database.py
@@ -0,0 +1,8 @@
+from app.core.config import get_settings
+
+
+settings = get_settings() 
+print(settings.database_url) 
+
+
+
--- app/main.py
+++ app/main.py
@@ -1,10 +1,8 @@
 from collections.abc import AsyncGenerator
-
 from fastapi import FastAPI
 from fastapi import APIRouter
 from app.core.config import get_settings
 from contextlib import asynccontextmanager
-
 
 @asynccontextmanager
 async  def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
